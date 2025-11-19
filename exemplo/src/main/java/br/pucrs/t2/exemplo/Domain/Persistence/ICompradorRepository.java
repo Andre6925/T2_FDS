a24c661eb9f74b2f3fe22fe943b4a43bb91227c2
Andre6925 <agoiquatrin@gmail.com>
2025-11-19 23:12:37 +0000

    interfaces finalizadas

--- exemplo/src/main/java/br/pucrs/t2/exemplo/Domain/Persistence/ICompradorRepository.java
+++ exemplo/src/main/java/br/pucrs/t2/exemplo/Domain/Persistence/ICompradorRepository.java
@@ -1,4 +1,12 @@
 package br.pucrs.t2.exemplo.Domain.Persistence;
 
+import java.util.List;
+import java.util.Optional;
+
+import br.pucrs.t2.exemplo.Domain.Entities.Comprador;
+
 public interface ICompradorRepository {
+    Optional<Comprador> getCompradores();
+    List<Comprador> getTecnologiaById(String pais);
+    boolean addComprador(Comprador comprador);
 }
--- exemplo/src/main/java/br/pucrs/t2/exemplo/Domain/Persistence/IFornecedorRepository.java
+++ exemplo/src/main/java/br/pucrs/t2/exemplo/Domain/Persistence/IFornecedorRepository.java
@@ -1,5 +1,13 @@
 package br.pucrs.t2.exemplo.Domain.Persistence;
 
-public class IFornecedorRepository {
-    
+import java.sql.Date;
+import java.util.List;
+import java.util.Optional;
+
+import br.pucrs.t2.exemplo.Domain.Entities.Fornecedor;
+
+public interface IFornecedorRepository {
+    Optional<Fornecedor> getFornecedores();
+    List<Fornecedor> getFornecedorById(Date dataFundacao);
+    boolean addFornecedor(Fornecedor fornecedor);
 }
