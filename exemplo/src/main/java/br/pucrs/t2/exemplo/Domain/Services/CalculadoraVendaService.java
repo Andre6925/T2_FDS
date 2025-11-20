aee0de10d0b2c6746b01ac839bd68269feaf1592
Andre6925 <agoiquatrin@gmail.com>
2025-11-20 02:28:47 +0000

    calculaVendaService

--- exemplo/src/main/java/br/pucrs/t2/exemplo/Domain/Services/CalculadoraVendaService.java
+++ exemplo/src/main/java/br/pucrs/t2/exemplo/Domain/Services/CalculadoraVendaService.java
@@ -1,5 +1,29 @@
 package br.pucrs.t2.exemplo.Domain.Services;
 
+import org.springframework.stereotype.Service;
+import br.pucrs.t2.exemplo.Domain.Entities.Venda;
+
+@Service
 public class CalculadoraVendaService {
     
+    public double calcularValorFinal(Venda venda, long quantidadeVendasAnteriores){
+        var tecnologia = venda.getTecnologia();
+        var fornecedor = tecnologia.getFornecedor();
+
+        double valorBase = tecnologia.getValorBase();
+
+        double acrescimoPercentual = switch (fornecedor.getArea()) {
+            case TI -> 0.20;
+            case ANDROIDES -> 0.15;
+            case EMERGENTE -> 0.25;
+            case ALIMENTOS -> 0.10;
+        };
+
+        double valorComAcrescimo = valorBase * (1 + acrescimoPercentual);
+
+        long descontoPercentual = Math.min(quantidadeVendasAnteriores, 10);
+        double fatorDesconto = 1 - (descontoPercentual / 100.0); 
+
+        return valorComAcrescimo * fatorDesconto;
+    }
 }
