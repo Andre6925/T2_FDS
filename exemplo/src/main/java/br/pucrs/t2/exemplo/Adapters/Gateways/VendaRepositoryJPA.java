a7291b0916366d67737e8785afbb350fbf4b9d08
Andre6925 <agoiquatrin@gmail.com>
2025-11-26 02:52:30 +0000

    finalizado

--- exemplo/src/main/java/br/pucrs/t2/exemplo/Adapters/Gateways/VendaRepositoryJPA.java
+++ exemplo/src/main/java/br/pucrs/t2/exemplo/Adapters/Gateways/VendaRepositoryJPA.java
@@ -6,8 +6,6 @@ import br.pucrs.t2.exemplo.Domain.Entities.Venda;
 
 public interface VendaRepositoryJPA extends CrudRepository<Venda, Long>{
     boolean existsByNum(Long num);
-    //Lista de compras - só ativas
     List<Venda> findByComprador_CodAndRemovidaFalse(Long codComprador);
-    //contagem para cálculo do desconto
     long countByComprador_CodAndRemovidaFalse(Long CodComprador);
 }
